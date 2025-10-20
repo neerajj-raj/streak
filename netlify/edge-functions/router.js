73ab4fdf3bbd4e1d83cff832f39e531fc8e7d3bd
Neeraj Raj <neeraj.r@valoriz.com>
2025-10-20 14:31:05 +0530

    router

---
+++ netlify/edge-functions/router.js
@@ -0,0 +1,44 @@
+export default async (request, context) => {
+  const url = new URL(request.url);
+
+  const referer = request?.headers?.get("referer") ?? null;
+  const refererUrl = referer ? new URL(referer) : null;
+
+  const streakRefererPaths = [
+    "/",
+    "/about",
+    "/about/",
+    "/assets/js/asset-worker.js",
+  ];
+  const streakConfiguredPathNames = ["/", "/about", "/about/"];
+
+  const isAsset =
+    /\.(png|jpe?g|gif|svg|webp|ico|css|js|woff2?|ttf|eot|json|vtt)$/i.test(
+      url.pathname
+    );
+
+  // Serve to Streak.js
+  if (
+    streakConfiguredPathNames.includes(url?.pathname) ||
+    (isAsset && streakRefererPaths?.includes(refererUrl?.pathname ?? "")) ||
+    refererUrl?.pathname?.includes("/secondary-css.css")
+  ) {
+    console.log(
+      `🪶 Requested url:"${url.pathname}" or referer:"${refererUrl?.pathname}" to Streak.js`
+    );
+    return context.next();
+  }
+
+  // Proxy everything else to Next.js
+  const NEXT_APP_DOMAIN = Deno.env.get("NEXT_APP_BASE_URL") ?? "";
+  const target = NEXT_APP_DOMAIN + url.pathname + url.search;
+  const response = await fetch(target, { headers: request.headers });
+
+  console.log(`➡️ Requested url:"${url.pathname + url.search}" to Next.js app`);
+
+  // Return proxied response
+  return new Response(response.body, {
+    status: response.status,
+    headers: response.headers,
+  });
+};
