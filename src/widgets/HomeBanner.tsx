4cd1a9b38b0491d8a6543da66364c1758ea834dd
Neeraj Raj <neeraj.r@valoriz.com>
2025-10-30 17:03:21 +0530

    revert

--- src/widgets/HomeBanner.tsx
+++ src/widgets/HomeBanner.tsx
@@ -29,7 +29,27 @@ const HomeBanner = (props: HomeBannerProps) => {
             fetchPriority="high"
             className="banner-bg"
           />
-
+          <div className="banner-text">
+            {banner1?.banner_heading && <h1>{banner1?.banner_heading}</h1>}
+            <p></p>
+            {banner1?.banner_content && <div dangerouslySetInnerHTML={{ __html: banner1?.banner_content }}></div>}
+            <p></p>
+            <div className="btn-flex">
+              {banner1?.button_name_1 &&
+                <a href={banner1?.button_link_1} className="btn blue" aria-label="contact-us">
+                  {banner1?.button_name_1}
+                </a>}
+              {banner1?.button_name_2 &&
+                // #8A4600 For LH Accessibility
+                <a href={banner1?.button_link_2} className="btn orange" aria-label="about-us" style={{ backgroundColor: "#8A4600" }}>
+                  {banner1?.button_name_2}
+                  {/* For SEO */}
+                  <span style={{ position: "absolute", width: "1px", height: "1px", padding: 0, margin: "-1px", overflow: "hidden", clip: "rect(0 0 0 0)", whiteSpace: "nowrap", border: 0, }}>
+                    about our company
+                  </span>
+                </a>}
+            </div>
+          </div>
         </div>
       </div>
     </div>
