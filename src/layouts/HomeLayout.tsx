d9a6c7ede75b04c8f802820bd330943011ea002f
Neeraj Raj <neeraj.r@valoriz.com>
2025-10-30 16:43:23 +0530

    t1

--- src/layouts/HomeLayout.tsx
+++ src/layouts/HomeLayout.tsx
@@ -11,7 +11,7 @@ const HomeLayout = () => {
         <main className="outer" role="main">
           <WidgetPlaceholder id="CommonHeader" type="CommonHeader" />
           <WidgetPlaceholder id="HomeBanner" type="HomeBanner" />
-          <WidgetPlaceholder id="AnimationHelpers" type="AnimationHelpers" />
+          {/* <WidgetPlaceholder id="AnimationHelpers" type="AnimationHelpers" />
           <WidgetPlaceholder id="HomeServices" type="HomeServices" />
           <WidgetPlaceholder id="HomeGallery" type="HomeGallery" />
           <WidgetPlaceholder id="HomeWhyChooseUs" type="HomeWhyChooseUs" />
@@ -19,7 +19,7 @@ const HomeLayout = () => {
           <WidgetPlaceholder id="HomeTeam" type="HomeTeam" />
           <WidgetPlaceholder id="HomeArticles" type="HomeArticles" />
           <WidgetPlaceholder id="CommonFooter" type="CommonFooter" />
-          <WidgetPlaceholder id="AnalyticsHelpers" type="AnalyticsHelpers" />
+          <WidgetPlaceholder id="AnalyticsHelpers" type="AnalyticsHelpers" /> */}
         </main>
       </body>
     </html>
