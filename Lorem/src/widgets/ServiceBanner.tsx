91a8d89addde8c96a17d725005bac59bcfd5f263
Neeraj Raj <neeraj.r@valoriz.com>
2025-11-25 15:52:50 +0530

    c

--- Lorem/src/widgets/ServiceBanner.tsx
+++ Lorem/src/widgets/ServiceBanner.tsx
@@ -25,13 +25,14 @@ const ServiceBanner = (props: ServiceBannerProps) => {
         <h1>{banner_heading ?? ""}</h1>
         <p></p>
         <span dangerouslySetInnerHTML={{ __html: banner_content ?? "" }} />
+        {/* For SEO */}
+        <h2 style={{ position: "absolute", width: "1px", height: "1px", padding: 0, margin: "-1px", overflow: "hidden", clip: "rect(0 0 0 0)", whiteSpace: "nowrap", border: 0, }}>{banner_content}</h2>
         <p></p>
         <a href={button_link ?? ""} className="btn blue">
           {button_name ?? ""}
         </a>
       </div>
     </div>
-
   )
 }
 
